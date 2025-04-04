8addc0653c6872500a2e6a52a61bc151cd99b6f1
Rafael Coli <rafacolialves@gmail.com>
2025-04-04 15:46:53 -0300

    Update

---
+++ neps/16.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> cofre(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> cofre[i];
+    }
+
+    return 0;
+}
