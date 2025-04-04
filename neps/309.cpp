4e3b224e3e09e9df69941e9d9173520c9b540752
RafaelColi <rafacolialves@gmail.com>
2025-04-04 13:20:06 -0300

    Busca em Grafo

--- neps/309.cpp
+++ neps/309.cpp
@@ -2,6 +2,60 @@
 
 using namespace std;
 
+int n, m;
+vector<vector<int>> graph;
+vector<bool> visited;
+
+int bl() {
+    int count = 0;
+    queue<int> q;
+
+    for(int i = 1; i < graph.size(); i++) {
+        if(!visited[i]) {
+            count++;
+            visited[i] = true;
+            q.push(i);
+
+            while(!q.empty()) {
+                int u = q.front();
+                q.pop();
+
+                for(int v : graph[u]) {
+                    if(!visited[v]) {
+                        visited[v] = true;
+                        q.push(v);
+                    }
+                }
+
+                visited[u] = true;
+            }
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    cin >> n >> m;
+    graph.resize(n + 1, vector<int> (n + 1));
+    visited.resize(n + 1, false);
+
+    for(int k = 1; k <= m; k++) {
+        int i, j;
+        cin >> i >> j;
+
+        graph[i].push_back(j);
+        graph[j].push_back(i);
+    }
+
+    cout << bl() << '\n';
+
+    return 0;
+}
+
+
+/*
+
 vector<int> pai;
 
 int find(int x) {
@@ -42,3 +96,5 @@ int main() {
 
     return 0;
 }
+
+*/
---
+++ neps/520.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, d;
+vector<vector<int>> trees;
+vector<bool> visited;
+
+void bl() {
+    int count = 1;
+    queue<int> q;
+    q.push(0);
+
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        for(int v : trees[u]) {
+            if(!visited[v]) {
+                visited[v] = true;
+                q.push(v);
+                count++;
+            }
+        }
+    }
+
+    if(count < n) 
+        printf("N\n");
+    else 
+        printf("S\n");
+    
+    return;
+}
+
+int main() {
+    scanf("%d %d", &n, &d);
+
+    vector<pair<int, int>> coord(n);
+    trees.resize(n);
+    visited.resize(n, false);
+
+    for(int i = 0; i < n; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+
+        coord[i] = {x, y};
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(i == j)
+                continue;
+
+            pair<int, int> u, v;
+            u = {coord[i].first, coord[i].second};
+            v = {coord[j].first, coord[j].second};
+
+            if((long double) sqrt((long double) pow(v.first - u.first, 2) + pow(v.second - u.second, 2)) <= d) {
+                trees[i].push_back(j);
+                trees[j].push_back(i);
+            }
+        }
+    }
+
+    bl();
+
+    return 0;
+}
